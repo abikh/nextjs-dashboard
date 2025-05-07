d6d15ea30f86c7263504c123a75ce8262797cbb9
Abid <abid@ajemdia.ae>
2025-05-07 12:04:12 +0400

    Merge local and remote histories

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,6 +2,7 @@ import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 export const lusitana = Lusitana({
-  subsets: ["latin"],
-  weight: ["400", "700"],
+  subsets: ["latin"], // ✅ specify at least one subset
+  weight: ["400", "700"], // or whatever weights you're using
+  preload: true, // this is true by default, but you can leave it in
 });
