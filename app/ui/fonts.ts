f42ed461852b552a401e4bd3f47021d3840fc54a
Abid <abid@ajemdia.ae>
2025-05-07 12:12:40 +0400

    Font Updated

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -4,5 +4,5 @@ export const inter = Inter({ subsets: ["latin"] });
 export const lusitana = Lusitana({
   subsets: ["latin"], // ✅ specify at least one subset
   weight: ["400", "700"], // or whatever weights you're using
-  preload: true, // this is true by default, but you can leave it in
+  preload: false, // this is true by default, but you can leave it in
 });
